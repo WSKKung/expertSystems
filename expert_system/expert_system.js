740666c1f9488125bf36d6e7bdf019849b00692c
WSKKung <armkung493@gmail.com>
2022-11-10 13:05:00 +0700

    Add main model endpoint

---
+++ expert_system/expert_system.js
@@ -0,0 +1,16 @@
+import { area, province, riceType } from "./variables.js"
+
+function getRiceBreedSuggestion({
+	riceType,
+	province,
+	inSeason,
+	area,
+	rainFrequency,
+	pests,
+	diseases
+}) {
+	
+
+
+
+}
