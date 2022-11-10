3abeae2fa98dc361b90d0bd38f3467c56bbdd6f9
WSKKung <armkung493@gmail.com>
2022-11-10 23:11:23 +0700

    Fixed middleware not found

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import { fullfillmentRequest } from "../controller/dialogflow";
+import { Router, json } from "express";
+import { fullfillmentRequest } from "../controller/dialogflow.js";
 const router = Router()
 
 router.get('/', (req, res) => {
 	res.status(200).send("Hello World!")
 })
   
-router.post('/api/dialogflow', express.json(), fullfillmentRequest)
+router.post('/api/dialogflow', json(), fullfillmentRequest)
 
 export {
 	router
