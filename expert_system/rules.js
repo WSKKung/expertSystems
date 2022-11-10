863f9da13ac4b0b79f2f864f2071e624d4323f6c
WSKKung <armkung493@gmail.com>
2022-11-10 23:10:17 +0700

    Refactor unused stuff & add exports

--- expert_system/rules.js
+++ expert_system/rules.js
@@ -1,89 +1,3 @@
-import { area, disease, pest, rainFrequency, riceType, waterLevel } from '../expert_system/variables.js'
-
-class Rule {
-	constructor(condition, resultFact) {
-		this.condition = condition
-		this.resultFact = resultFact
-	}
-	
-	apply(obj) {
-		if (this.condition.test(obj)) {
-			Object.keys(this.resultFact).forEach((key) => {
-				obj[key] = this.resultFact[key]
-			})
-			return true
-		}
-		return false
-	}
-}
-
-class RuleBuilder {
-
-	constructor(initialCondition) {
-		this.condition = initialCondition
-	}
-
-	static if(props) {
-		return new this(new AllMatchCondition(props))
-	}
-
-	static ifNot(props) {
-		return new this(new InverseCondition(new AllMatchCondition(props)))
-	}
-
-	static ifIn(props) {
-		return new this(new AllIncludeCondition(props))
-	}
-
-	static ifNotIn(props) {
-		return new this(new InverseCondition(new AllIncludeCondition(props)))
-	}
-
-	and(props) {
-		this.condition = this.condition.and(new AllMatchCondition(props))
-		return this
-	}
-
-	andIn(props) {
-		this.condition = this.condition.and(new AllIncludeCondition(props))
-		return this
-	}
-
-	or(props) {
-		this.condition = this.condition.or(new AllMatchCondition(props))
-		return this
-	}
-
-	orIn(props) {
-		this.condition = this.condition.or(new AllIncludeCondition(props))
-		return this
-	}
-
-	andNot(props) {
-		this.condition = this.condition.andNot(new AllMatchCondition(props))
-		return this
-	}
-
-	andNotIn(props) {
-		this.condition = this.condition.andNot(new AllIncludeCondition(props))
-		return this
-	}
-
-	orNot(props) {
-		this.condition = this.condition.orNot(new AllMatchCondition(props))
-		return this
-	}
-
-	orNotIn(props) {
-		this.condition = this.condition.orNot(new AllIncludeCondition(props))
-		return this
-	}
-
-	then(facts) {
-		return new Rule(this.condition, facts)
-	}
-}
-
 class Condition {
 	test(obj) {
 		throw Error('test function is not implemented!')
@@ -106,20 +20,6 @@ class Condition {
 	}
 }
 
-class IfEqualCondition extends Condition {
-
-	constructor(propName, value) {
-		super()
-		this.propName = propName
-		this.value = value
-	}
-
-	test(obj) {
-		return obj[this.propName] === this.value
-	}
-
-}
-
 // match object กับทุก properties ใน props 
 // ถ้ามีบางตัวที่ค่าไม่ตรงจะตอบ false ทันที
 // e.g.
@@ -127,14 +27,14 @@ class IfEqualCondition extends Condition {
 // new AllMatchCondition({ a: 1, b: 2 }).test({ a: 3, b: 1 }) == false
 // new AllMatchCondition({ a: 1, b: 2 }).test({ b: 2 }) == false
 // new AllMatchCondition({ a: 1, b: 2 }).test({ a: 1, b: 2 }) == true
-class AllMatchCondition extends Condition {
+export class AllMatchCondition extends Condition {
 
 	/*
 	* props: object ที่เก็บ properties ที่จะให้ตรวจสอบทั้งหมด
 	* strict: flag ที่เมื่อ set จะตรวจสอบ properties โดยใช้ === แทน ==
 	* useIncludes: flag ที่เมื่อ set จะใช้ method includes ในการตรวจสอบหากค่าของ properties ใน props มี method นั้นๆ
 	*/
-	constructor(props, strict = true, useIncludes = false) {
+	constructor(props, strict = true, useIncludes = true) {
 		super()
 		this.props = props
 		
@@ -154,7 +54,7 @@ class AllMatchCondition extends Condition {
 
 	test(obj) {
 		let matchingKeys = Object.keys(this.props)
-		return matchingKeys.every(key => obj[key] === this.props[key])
+		return matchingKeys.every(key => this.propValueComparer(this.props[key], obj[key])  )
 	}
 
 }
@@ -164,7 +64,7 @@ class AllMatchCondition extends Condition {
 // ถ้ามีค่าใน object บางตัวที่ไม่อยู่ใน array ของค่าใน props จะตอบ false ทันที
 // new AllIncludeCondition({ a: [1, 2, 3] }).test({ a: 4 }) == false
 // new AllIncludeCondition({ a: [1, 2, 3] }).test({ a: 2 }) == true
-class AllIncludeCondition extends Condition {
+export class AllIncludeCondition extends Condition {
 	
 	constructor(props) {
 		super()
@@ -177,7 +77,7 @@ class AllIncludeCondition extends Condition {
 	}
 }
 
-class InverseCondition extends Condition {
+export class InverseCondition extends Condition {
 	constructor(baseCondition) {
 		super()
 		this.baseCondition = baseCondition
@@ -188,7 +88,7 @@ class InverseCondition extends Condition {
 	}
 }
 
-class AndCondition extends Condition {
+export class AndCondition extends Condition {
 
 	constructor(conditions) {
 		super()
@@ -205,7 +105,7 @@ class AndCondition extends Condition {
 
 }
 
-class OrCondition extends Condition {
+export class OrCondition extends Condition {
 
 	constructor(conditions) {
 		super()
@@ -219,41 +119,4 @@ class OrCondition extends Condition {
 	or(other) {
 		return new OrCondition(this.conditions.concat([ other ]))
 	}
-}
-
-const rules = []
-
-function addRule(rule) {
-	rules.push(rule)
-}
-
-addRule(RuleBuilder
-	.if({ area: area.irrigatedLowland })
-	.or({ rainFrequency: rainFrequency.average })
-	.then({ waterLevel: waterLevel.average })
-)
-
-addRule(RuleBuilder
-	.ifNot({ area: area.irrigatedLowland })
-	.and({ rainFrequency: rainFrequency.low })
-	.then({ waterLevel: waterLevel.low })
-)
-
-addRule(RuleBuilder
-	.ifNot({ area: area.irrigatedLowland })
-	.and({ rainFrequency: rainFrequency.high })
-	.orIn({ area: [ area.floating, area.deepwater ] })
-	.then({ waterLevel: waterLevel.high })
-)
-
-addRule(RuleBuilder
-	.if({ riceType: riceType.paddy })
-	.and({ area: area.irrigatedLowland	})
-	.andIn({ pest: [ pest.greenLeafhopper ] })
-	.andIn({ disease: [ disease.brownSpot ] })
-	.then({ riceBreed: "กข1" })
-)
-
-export default {
-	rules
 }
