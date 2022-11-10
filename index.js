762aa3dd2ed61ee49ec270e0d615db96ccd406f4
WSKKung <armkung493@gmail.com>
2022-11-10 23:08:47 +0700

    Added path & url dependencies

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import { router } from './routes/router.js'
+import { publicFolder } from './util/path.js'
 
 const app = express()
 
 app.use(router)
 
 const port = 8081
+
+// Expose public folder for any requests
+app.use(express.static(publicFolder))
 
 app.listen(port, () => {
   console.log(`listening on port http://localhost:${port}`)
