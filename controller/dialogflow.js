79c64637e854843a05c0dbec1697fe28986fc1b3
WSKKung <armkung493@gmail.com>
2022-11-11 12:49:29 +0700

    Fixed import, add intent handler

--- controller/dialogflow.js
+++ controller/dialogflow.js
@@ -1,15 +1,20 @@
+import { WebhookClient } from "dialogflow-fulfillment"
+
 // Accept fulfillment request from Dialogflow
 export function fullfillmentRequest(req, res) {
 	let agent = new WebhookClient({ request: req, response: res })
-
 	let intentMap = new Map()
 	intentMap.set("Rude word", rudeWord)
-
+	intentMap.set("แนะนำข้าว", suggestRice)
 	agent.handleRequest(intentMap)
-
+	
 }
 
 // Intent handlers
 function rudeWord(agent) {
 	agent.add("Voraphat")
+}
+
+function suggestRice(agent) {
+	agent.add(agent.consoleMessages)
 }
