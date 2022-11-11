b0277accd9a4d484fd275214369084a5e2276722
Sahachai Plangrit <31693785+WSKKung@users.noreply.github.com>
2022-11-11 08:03:23 +0000

    Add rice suggest fulfillment logic

--- controller/dialogflow.js
+++ controller/dialogflow.js
@@ -1,4 +1,6 @@
 import { WebhookClient } from "dialogflow-fulfillment"
+import { area } from "../expert_system/variables.js"
+import { getRiceBreedSuggestion } from "../expert_system/expert_system.js"
 
 // Accept fulfillment request from Dialogflow
 export function fullfillmentRequest(req, res) {
@@ -12,9 +14,55 @@ export function fullfillmentRequest(req, res) {
 
 // Intent handlers
 function rudeWord(agent) {
-	agent.add("Voraphat")
+	agent.add(agent.consoleMessages)
 }
 
 function suggestRice(agent) {
-	agent.add(agent.consoleMessages)
+  
+  let params = agent.parameters
+
+  console.log(params)
+  
+  // default to DialogFlow console messages if strictly required params are missing
+  if (!params.riceType || !params.province || !params.season || !params.riceArea ) {
+    agent.add(agent.consoleMessages)
+    return
+  }
+
+  // rain frequency can be ignored on the following area
+  if ( [ area.irrigatedLowland, area.floating, area.deepwater ].includes(params.riceArea) ) {
+    let factor = {
+      riceType: params.riceType,
+      province: params.province,
+      inSeason: params.season,
+      area: params.riceArea
+    }
+    let suggestionResults = getRiceBreedSuggestion(factor)
+    agent.add(suggestionResults.map(rice => rice.name).join(', '))
+    clearAgentContexts(agent)
+    return 
+  }
+
+  if (params.rainFrequency) {
+    let factor = {
+      riceType: params.riceType,
+      province: params.province,
+      inSeason: params.season,
+      area: params.riceArea,
+      rainFrequency: params.rainFrequency
+    }
+    let suggestionResults = getRiceBreedSuggestion(factor)
+    agent.add(suggestionResults.map(rice => rice.name).join(', '))
+    clearAgentContexts(agent)
+    return
+  }
+  
+  agent.add(agent.consoleMessages)
+	
+}
+
+function clearAgentContexts(agent) {
+    for (let ctx of agent.context) {
+      agent.context.delete(ctx.name)
+    }
 }
