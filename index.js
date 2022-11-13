023f7ae2073977aa8c757aa3f5c0f615e860e5d7
Sahachai Plangrit <31693785+WSKKung@users.noreply.github.com>
2022-11-13 04:20:35 +0000

    Added cors

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import { router } from './routes/router.js'
 import { publicFolder } from './util/path.js'
+import cors from 'cors'
 
 const app = express()
+const port = 8081
 
 app.use(router)
 
-const port = 8081
-
 // Expose public folder for any requests
-app.use(express.static(publicFolder))
+app.use(express.static(publicFolder, { extensions: ['html', 'png'] }))
+app.use(cors())
 
 app.listen(port, () => {
   console.log(process.env.hostname)
