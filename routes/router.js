d101a6c3022913b4b0b5677d425163fe3afc1667
WSKKung <armkung493@gmail.com>
2022-11-13 21:58:46 +0700

    Deleted default path route

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,6 @@
 import { Router, json } from "express";
 import { fullfillmentRequest } from "../controller/dialogflow/webhook.js";
 const router = Router()
-
-router.get('/', (req, res) => {
-	res.status(200).send("Hello World!")
-})
   
 router.post('/api/dialogflow', json(), fullfillmentRequest)
 
