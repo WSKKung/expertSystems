787c4e11da68cad75d708fd413c83881f81b15a5
WSKKung <armkung493@gmail.com>
2022-11-13 11:50:30 +0700

    Fixed public url to allow / in filePath

--- util/path.js
+++ util/path.js
@@ -1,11 +1,12 @@
 import { dirname, join, resolve } from 'path'
-import { fileURLToPath, pathToFileURL } from 'url'
+import { fileURLToPath, pathToFileURL, URL } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export const root = resolve(__dirname, '..')
 export const publicFolder = resolve(root, 'public')
+export const hostURL = new URL(process.env.hostname || 'http://localhost:8081')
 
 /**
  * 
@@ -13,5 +14,6 @@ export const publicFolder = resolve(root, 'public')
  * @returns {URL} Full URL to file
  */
 export function publicFileURL(filePath) {
-  return process.env.hostname + "/" + filePath
+  let path = join("/", filePath)
+  return new URL(path, hostURL)
 }
